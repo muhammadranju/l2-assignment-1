025f2a95b8a73d1e4e52753431bbc11249b2cf25
Muhammad Ranju <mdranju23@gmail.com>
2025-05-07 23:52:29 +0600

    add Vehicle and Car classes with methods for vehicle information

--- index.ts
+++ index.ts
@@ -15,3 +15,30 @@ function filterByRating(
 function concatenateArrays<T>(...arrays: T[][]): T[] {
   return [...arrays.flat()];
 }
+
+class Vehicle {
+  private make: string;
+  year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfo(): string {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
