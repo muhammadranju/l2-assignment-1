ddfb3b410072d51bbf428f8cc332ac8a81f91cc6
Muhammad Ranju <mdranju23@gmail.com>
2025-05-07 23:52:53 +0600

    add getMostExpensiveProduct function to retrieve the highest priced product

--- index.ts
+++ index.ts
@@ -50,3 +50,22 @@ function processValue(value: string | number): number {
     return value * 2;
   }
 }
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0) return null;
+
+  let mostExpensiveProduct = products[0];
+
+  for (const product of products) {
+    if (product.price > mostExpensiveProduct.price) {
+      mostExpensiveProduct = product;
+    }
+  }
+
+  return mostExpensiveProduct;
+}
