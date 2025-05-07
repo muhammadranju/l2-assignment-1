cf3ae6fd8ec209791b43c0f123609228afdc44b1
Muhammad Ranju <mdranju23@gmail.com>
2025-05-07 23:52:01 +0600

    add filterByRating function to filter items by rating

--- index.ts
+++ index.ts
@@ -5,3 +5,9 @@ function formatString(input: string, toUpper?: boolean): string {
 
   return input.toLowerCase();
 }
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating > 4);
+}
