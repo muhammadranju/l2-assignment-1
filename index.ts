e37e3b3c8ef787058b1ee4d9b3de18cf127ef374
Muhammad Ranju <mdranju23@gmail.com>
2025-05-07 23:52:12 +0600

    add concatenateArrays function to merge multiple arrays

--- index.ts
+++ index.ts
@@ -11,3 +11,7 @@ function filterByRating(
 ): { title: string; rating: number }[] {
   return items.filter((item) => item.rating > 4);
 }
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return [...arrays.flat()];
+}
