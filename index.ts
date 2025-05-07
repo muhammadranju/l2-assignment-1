5a929b1b816b2564d8a0951b1eedcbd7400dffaa
Muhammad Ranju <mdranju23@gmail.com>
2025-05-07 23:51:41 +0600

    implement formatString function to convert input string case

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || toUpper === undefined) {
+    return input.toUpperCase();
+  }
 
+  return input.toLowerCase();
+}
